d43037015a4122a7538a7a2e816d8da40948cce1
Kevyn Bruyere <kevynb@users.noreply.github.com>
2021-09-28 09:57:33 +0200

    feat: cleanup tunnel when starting

--- cloudflareTunnel/run.sh
+++ cloudflareTunnel/run.sh
@@ -4,9 +4,16 @@ TUNNEL_NAME="$(bashio::config 'tunnelName')"
 LOCAL_URL="$(bashio::config 'localUrl')"
 HOSTNAME="$(bashio::config 'hostname')"
 
+
+
 if [ ! -f "/root/.cloudflared/cert.pem" ]; then
+    echo "Cert file does not exists. Logging in."
     cloudflared tunnel login
+    echo "Logged in, cleanup pre-existing tunnels."
+    cloudflared tunnel cleanup ${TUNNEL_NAME}
+    echo "Deleting pre-existing tunnels."
     cloudflared tunnel delete ${TUNNEL_NAME}
+    echo "Tunnel ${TUNNEL_NAME} deleted."
 fi
 
 cloudflared tunnel --name ${TUNNEL_NAME}  --url ${LOCAL_URL} --hostname ${HOSTNAME}
