a8e40b0280e8387113eaca5a694457a4c5b08215
Kevyn Bruyere <kevynb@users.noreply.github.com>
2021-09-28 18:12:19 +0200

    feat: add more env variables for persistence

--- cloudflareTunnel/run.sh
+++ cloudflareTunnel/run.sh
@@ -3,14 +3,15 @@
 TUNNEL_NAME="$(bashio::config 'tunnelName')"
 LOCAL_URL="$(bashio::config 'localUrl')"
 HOSTNAME="$(bashio::config 'hostname')"
-CONFIG_DIR="data/config.yml"
-
+CONFIG_DIR="data"
+CONFIG_FILE="${CONFIG_DIR}/config.yml"
+TUNNEL_ORIGIN_CERT=${CONFIG_DIR}/cert/pem
 
 bashio::log.info ls /root/.cloudflared/cert.pem
 ls /root/.cloudflared/cert.pem || bashio::log.info 'ls failed'
 
 
-if [ ! bashio::fs.file_exists "/root/.cloudflared/cert.pem" ]; then
+if ! bashio::fs.file_exists "/root/.cloudflared/cert.pem" ; then
     bashio::log.info "Cert file does not exists. Logging in."
     cloudflared tunnel login
     bashio::log.info "Logged in, cleanup pre-existing tunnels."
@@ -20,4 +21,4 @@ if [ ! bashio::fs.file_exists "/root/.cloudflared/cert.pem" ]; then
     bashio::log.info "Tunnel ${TUNNEL_NAME} deleted."
 fi
 
-cloudflared tunnel --config ${CONFIG_DIR} --name ${TUNNEL_NAME}  --url ${LOCAL_URL} --hostname ${HOSTNAME} --overwrite-dns
+cloudflared tunnel --config ${CONFIG_FILE} --name ${TUNNEL_NAME}  --url ${LOCAL_URL} --hostname ${HOSTNAME} --overwrite-dns
